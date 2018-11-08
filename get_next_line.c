0d8a182a294ce5b4884c5c0b4fc907f39237c3a8
Quentin Backaert <qbackaer@w-r9-p6.s19.be>
2018-11-08 18:42:09 +0100

    works

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl3.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qbackaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/25 21:00:43 by qbackaer          #+#    #+#             */
-/*   Updated: 2018/11/05 17:38:41 by qbackaer         ###   ########.fr       */
+/*   Created: 2018/11/07 20:00:19 by qbackaer          #+#    #+#             */
+/*   Updated: 2018/11/08 18:40:47 by qbackaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,101 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "header.h"
 #include "libft.h"
-#include <string.h>
+
+char	*read_until(const int fd, char **leftover)
+{
+	char	*rd_buff;
+	char	*ln_buff;
+	size_t	ret;
+	char	*chr;
+
+	rd_buff = ft_strnew(BUFF_SIZE);
+	ln_buff = ft_strnew(0);
+	chr = NULL;
+	while ((ret = read(fd, rd_buff, BUFF_SIZE)) > 0 && !ft_strchr(rd_buff, SPR))
+	{
+		rd_buff[ret] = '\0';
+		ln_buff = ft_strjoin(ln_buff, rd_buff);
+		ft_bzero(rd_buff, ft_strlen(rd_buff) + 1);
+	}
+	if (ret > 0)
+		ln_buff = ft_strjoin(ln_buff, rd_buff);
+	ft_bzero(rd_buff, ft_strlen(rd_buff) + 1);
+	if ((chr = ft_strchr(ln_buff, SPR)))
+	{
+		if (ret > 1)
+			*leftover = ft_strdup(chr + 1);
+		*chr = '\0';
+	}
+	return (ln_buff);
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	char *tmp;
+	char		*buffer;
+	static char	*leftover;
+	char		*lo_chr;
 
-	if (!line || fd < 0)
+	if (!fd || !line || BUFF_SIZE < 1)
 		return (-1);
-	if (!(tmp = malloc(BUFF_SIZE + 1)) || !(*line = malloc(BUFF_SIZE + 1)))
-			return (-1);
-	while (strchr(tmp, '\n'))
+	buffer = ft_strnew(0);
+	if (leftover)
 	{
-		read(1, tmp, BUFF_SIZE);
+		buffer = ft_strjoin(buffer, leftover);
+		if ((lo_chr = ft_strchr(buffer, SPR)))
+		{
+			leftover = ft_strdup(lo_chr + 1);
+			*lo_chr = '\0';
+			*line = buffer;
+			return (1);
+		}
+		else
+			free(leftover);
 	}
-	strcpy(*line, tmp);
+	buffer = ft_strjoin(buffer, read_until(fd, &leftover));
+	*line = buffer;
+	if (!ft_strlen(*line))
+		return (0);
 	return (1);
 }
 
-int		main(int argc, char **argv)
+int		main(void)
 {
-	int fd;
-	char **line;
+	int		fd;
+	char	*line;
+	int		ret = 1;
 
-	line = malloc(2);
-	*line = malloc(10000);
-	strcpy (*line, "ok!");
-	if (argc == 2)
+	while (true)
 	{
-		fd = open(argv[1], O_RDONLY);
-		get_next_line(fd, line);
+		fd = open("test", O_RDONLY);
+		while (ret == 1)
+		{
+			ret = get_next_line(fd, &line);
+			printf("%d.>>%s<<\n", ret, line);
+		}
+		close(fd);
 	}
-	printf("%s\n", *line);
+	/*ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);
+	ret = get_next_line(fd, &line);
+	printf("%d.>>%s<<\n", ret, line);*/
+	
 	return (0);
 }
--- get_next_line2.c
+++ get_next_line2.c
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qbackaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/24 19:41:42 by qbackaer          #+#    #+#             */
-/*   Updated: 2018/11/08 16:11:23 by qbackaer         ###   ########.fr       */
+/*   Created: 2018/11/08 18:26:45 by qbackaer          #+#    #+#             */
+/*   Updated: 2018/11/08 18:30:07 by qbackaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "header.h"
+#include "libft.h"
 
-# define BUFF_SIZE 37
-# define SPR '\n'
+int		get_next_line(const int fd, char **line)
+{
 
-int		get_next_line(const int fd, char **line);
-
-#endif
+}
